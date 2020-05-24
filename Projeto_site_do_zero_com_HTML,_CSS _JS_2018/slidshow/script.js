a767e3ba40947f5eb105d7f5d446966fe217d5e6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-24 10:18:23 +0100

    Teste de slidshw

--- Projeto_site_do_zero_com_HTML,_CSS _JS_2018/slidshow/script.js
+++ Projeto_site_do_zero_com_HTML,_CSS _JS_2018/slidshow/script.js
@@ -24,3 +24,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+var tslideIndex = 0;
+
+function tshowSlides() {
+  var i; 
+  var slides = document.getElementsByClassName("mySlides"); 
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  tslideIndex++;
+  if (tslideIndex > slides.length) {tslideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[tslideIndex-1].style.display = "block";  
+  dots[tslideIndex-1].className += " active";
+  setTimeout(tshowSlides, 2000); // Change image every 2 seconds
+}
